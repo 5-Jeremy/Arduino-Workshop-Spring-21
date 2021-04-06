b40a70f07a010d36860346d9904df5ce399921e5
Jeremy Carleton <56325013+5-Jeremy@users.noreply.github.com>
2021-04-06 11:49:57 -0700

    Fix typo

--- Session 0/S0_LED_Control/S0_LED_Control.ino
+++ Session 0/S0_LED_Control/S0_LED_Control.ino
@@ -8,7 +8,7 @@ void loop()
 {
   // Because the input to pin 2 is always connected to
   // the pullup resistor (via the pushbutton), we recieve 
-  // high voltage on pin 2 when the button is button is up.
+  // high voltage on pin 2 when the button is up.
   // When the button is down, the wire connecting pin 2 to
   // the pullup resistor is grounded, so we recieve
   // low voltage on pin 2. For this reason, we take the
